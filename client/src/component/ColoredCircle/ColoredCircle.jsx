f4e7cd3590b9361e55dde875eccd2aafe7255a17
Mikalay05 <130233817+Mikalay05@users.noreply.github.com>
2024-12-13 16:58:53 +0300

    feat:  implementation of functionality in the ColoredCircle component

--- client/src/component/ColoredCircle/ColoredCircle.jsx
+++ client/src/component/ColoredCircle/ColoredCircle.jsx
@@ -1,12 +1,21 @@
-import "./ColoredCircle.css"
+import "./ColoredCircle.css";
 
 export default function ColoredCircle({
-
+    size = "15px",
+    colorCircle = { r: 0, g: 0, b: 0 }
 }) {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+    // Функция для преобразования RGB в строку
+    const rgbColor = `rgb(${colorCircle.r}, ${colorCircle.g}, ${colorCircle.b})`;
 
+    return (
+        <div
+            className="colored-circle"
+            style={{
+                width: size,
+                height: size,
+                backgroundColor: rgbColor,
+                borderRadius: '50%', // Делает круг
+            }}
+        />
+    );
+}
