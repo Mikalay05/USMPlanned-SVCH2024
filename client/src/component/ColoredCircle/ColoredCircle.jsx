aaa95fa5aedb9193e9d8d31c510f5d6a93e9a66e
Mikalay05 <130233817+Mikalay05@users.noreply.github.com>
2024-12-13 16:56:10 +0300

    feat: add ColoredCircle component

---
+++ client/src/component/ColoredCircle/ColoredCircle.jsx
@@ -0,0 +1,12 @@
+import "./ColoredCircle.css"
+
+export default function ColoredCircle({
+
+}) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
--- client/src/component/TaskStatus/TaskStatus.jsx
+++ client/src/component/TaskStatus/TaskStatus.jsx
@@ -1,9 +1,16 @@
 import "./TaskStatus.css"
 
+const typesDataComponent = [
+    "data",
+    "choose"
+]
 export default function TaskStatus({
-
+    type = typesDataComponent[0],
+    taskStatus
 }) {
     return (
-        <></>
+        <>
+            <div>
+        </>
     )
 }
