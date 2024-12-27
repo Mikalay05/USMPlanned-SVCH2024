77e8abb33111eab0fde58b8dba93410a8d76bd9c
Mikalay05 <130233817+Mikalay05@users.noreply.github.com>
2024-12-27 17:29:25 +0300

    feat: styles of button in slider

--- client/src/component/CustomerSlider/CustomerSlider.jsx
+++ client/src/component/CustomerSlider/CustomerSlider.jsx
@@ -1,23 +1,28 @@
-import React, { useState } from 'react';
-import './CustomerSlider.css';
-import CustomerCard from '../CustomerCard/CustomerCard';
+import React, { useState } from "react";
+import "./CustomerSlider.css";
+import CustomerCard from "../CustomerCard/CustomerCard";
 
 export default function CustomerSlider({
   children, // Дочерние элементы (массив)
   alphaInactiveOnEmtyElement = "0.1",
   emptyCardComponent = CustomerCard, // Дефолтный компонент пустой карточки
   onClickOnEmptyElementInSlider,
+  nameOfSliderIndexFile = "Icon-SliderIndex.svg",
 }) {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   // Функция для переключения на предыдущий элемент
   const handlePrev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? children.length - 1 : prevIndex - 1));
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? children.length - 1 : prevIndex - 1
+    );
   };
 
   // Функция для переключения на следующий элемент
   const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === children.length - 1 ? 0 : prevIndex + 1));
+    setCurrentIndex((prevIndex) =>
+      prevIndex === children.length - 1 ? 0 : prevIndex + 1
+    );
   };
 
   // Проверка, активна ли кнопка назад
@@ -28,7 +33,11 @@ export default function CustomerSlider({
 
   // Создание пустого слайда
   const createEmptyCard = () => {
-    return React.createElement(emptyCardComponent, { alphaInactive: alphaInactiveOnEmtyElement, isEmpty: true, onClickOnEmptyElement: onClickOnEmptyElementInSlider });
+    return React.createElement(emptyCardComponent, {
+      alphaInactive: alphaInactiveOnEmtyElement,
+      isEmpty: true,
+      onClickOnEmptyElement: onClickOnEmptyElementInSlider,
+    });
   };
 
   // Получение данных слайда по индексу
@@ -51,13 +60,19 @@ export default function CustomerSlider({
         onClick={handlePrev}
         disabled={isPrevDisabled} // Отключаем кнопку, если это первый элемент
       >
-        ←
+        <img
+          className="left-slider-index"
+          src={`/${nameOfSliderIndexFile}`}
+          alt="Left"
+        />
       </button>
 
       <div className="slider">
-        {getDataByIndex(currentIndex - 1, false)} {/* Пассивный элемент слева */}
+        {getDataByIndex(currentIndex - 1, false)}{" "}
+        {/* Пассивный элемент слева */}
         {getDataByIndex(currentIndex, true)} {/* Активный элемент в центре */}
-        {getDataByIndex(currentIndex + 1, false)} {/* Пассивный элемент справа */}
+        {getDataByIndex(currentIndex + 1, false)}{" "}
+        {/* Пассивный элемент справа */}
       </div>
 
       {/* Кнопка вперед (дезактивирована, если currentIndex === последний элемент) */}
@@ -66,7 +81,11 @@ export default function CustomerSlider({
         onClick={handleNext}
         disabled={isNextDisabled} // Отключаем кнопку, если это последний элемент
       >
-        →
+        <img
+          className="right-slider-index"
+          src={`/${nameOfSliderIndexFile}`}
+          alt="Right"
+        />
       </button>
     </div>
   );
