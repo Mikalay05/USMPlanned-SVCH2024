e2d1202701f3b7ace47d6df89f6b88936c81aac7
Mikalay05 <130233817+Mikalay05@users.noreply.github.com>
2025-01-13 01:37:29 +0300

    feat: add interceptors for login

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -9,4 +9,32 @@ const $api = axios.create({
     baseURL: API_URL
 })
 
+$api.interceptors.request.use((config) => {
+    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+    return config;
+})
+
+$api.interceptors.request.use((config) =>{
+    return config;
+}, async (error) => {
+    const originalRequest = error.config;
+
+    if(error.response.status == 401 && error.config && !error.config._isRetry){
+        originalRequest._isRetry = true;
+
+        try{
+            console.log("Пробую получить assecc")
+            const response = await axios.get(`${API_URL}/user/updateToken`, {withCredentials: true})
+            localStorage.setItem('token', response.data.accessToken);
+
+            return $api.request(originalRequest);
+        } catch(e){
+            console.log('не авторизован')
+        }
+
+    }
+
+    throw error;
+})
+
 export default $api;
