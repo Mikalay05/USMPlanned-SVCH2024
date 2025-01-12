209937ba269588334c9f2feca57566bd5b7fbcfb
Mikalay05 <130233817+Mikalay05@users.noreply.github.com>
2025-01-13 01:29:20 +0300

    fix: authmiddleware fixed next in  method getAuthTokenFromHeaders

--- server/middleware/AuthMiddleware.js
+++ server/middleware/AuthMiddleware.js
@@ -2,20 +2,20 @@ const ApiError = require("../error/ApiError");
 const TokenService = require("../services/TokenService");
 
 const getAuthTokenFromHeaders = (req) => {
-    const authToken = req.headers.authorization; 
-    if (!authToken || !authToken.startsWith('Bearer ')) {
-        return next(ApiError.unauthorized('Authorization token is missing or invalid'));
+    const authToken = req.headers.authorization;
+    if (!authToken || !authToken.startsWith("Bearer ")) {
+        throw ApiError.unauthorized("Authorization token is missing or invalid");
     }
-    const valueAuthToken = authToken.split(' ')[1];
+    const valueAuthToken = authToken.split(" ")[1];
     return TokenService.validateAccessToken(valueAuthToken);
-}
+};
 
 module.exports = function (req, res, next) {
     try {
-        const data =  getAuthTokenFromHeaders(req);
+        const data = getAuthTokenFromHeaders(req);
 
         if (!data) {
-            return next(ApiError.unauthorized('Invalid or expired token'));
+            return next(ApiError.unauthorized("Invalid or expired token"));
         }
 
         req.userIdFromToken = data.id;
@@ -23,7 +23,7 @@ module.exports = function (req, res, next) {
 
         next();
     } catch (err) {
-        console.log("ERROR in AuthMiddleware:",err)
-        return next(ApiError.unauthorized('An error occurred during token validation', err));
+        console.error("ERROR in AuthMiddleware:", err);
+        return next(err instanceof ApiError ? err : ApiError.unauthorized("An error occurred during token validation", err));
     }
 };
