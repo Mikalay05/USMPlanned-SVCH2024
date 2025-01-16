6cc8570db3a25735d5f2ad7712459f5a053b3ee8
Mikalay05 <130233817+Mikalay05@users.noreply.github.com>
2025-01-17 02:20:34 +0300

    feat: add delete modal in project information

--- client/src/component/ProjectDetails/ProjectDetails.jsx
+++ client/src/component/ProjectDetails/ProjectDetails.jsx
@@ -6,11 +6,11 @@ import { useState } from "react";
 // Функция для форматирования даты
 const formatDate = (dateString) => {
   const date = new Date(dateString);
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
   const year = date.getFullYear();
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
 
   return `${day}.${month}.${year} ${hours}:${minutes}`;
 };
@@ -19,11 +19,14 @@ export default function ProjectDetails({ projectData }) {
   const [openDeleteModal, setOpenDeleteModal] = useState(false);
   const handleOnOpenDeleteModal = () => {
     setOpenDeleteModal(true);
-  }
+  };
   const handleOnCloseDeleteModal = () => {
     setOpenDeleteModal(false);
+  };
+  const handleOnConfirm = () => {
+    //TODO Delete project request
+    setOpenDeleteModal(false);
   }
-  console.log(openDeleteModal)
   return (
     <div className="project-details-container">
       <div className="project-description">
@@ -32,7 +35,10 @@ export default function ProjectDetails({ projectData }) {
       </div>
       <div className="project-actions-buttons">
         <CustomerButton textValue="Change project" />
-        <CustomerButton textValue="Delete project" onClick={handleOnOpenDeleteModal} />
+        <CustomerButton
+          textValue="Delete project"
+          onClick={handleOnOpenDeleteModal}
+        />
       </div>
       <div className="project-actions-section">
         <h3 className="project-actions-title">Actions of Project:</h3>
@@ -40,7 +46,8 @@ export default function ProjectDetails({ projectData }) {
           {projectData.actions.length > 0 ? (
             projectData.actions.map((action) => (
               <p key={action.actionId} className="project-action-item">
-                {formatDate(action.createdAt)} | {action.user.login}({action.user.fullName}) did {action.actionName}
+                {formatDate(action.createdAt)} | {action.user.login}(
+                {action.user.fullName}) did {action.actionName}
               </p>
             ))
           ) : (
@@ -48,7 +55,15 @@ export default function ProjectDetails({ projectData }) {
           )}
         </div>
       </div>
-      <ConfirmationModal isOpen={openDeleteModal} onCancel={handleOnCloseDeleteModal}/>
+      <ConfirmationModal
+        isOpen={openDeleteModal}
+        title="Do you want to delete the project?"
+        message="All internal objects will be removed!"
+        confirmButtonText="Yes, delete it."
+        cancelButtonText="No, to go back"
+        onConfirm={handleOnConfirm}
+        onCancel={handleOnCloseDeleteModal}
+      />
     </div>
   );
 }
